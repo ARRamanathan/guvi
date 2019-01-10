6b679f6033ae5d75cf9fec74223772dbaa3aa067
ARRamanathan <ARRamanathan@users.noreply.github.com>
2019-01-10 22:35:27 +0530

    Add files via upload

---
+++ prog4.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Ramanathan AR
+#
+# Created:     10/01/2019
+# Copyright:   (c) Ramanathan AR 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+case= input('')
+alphabet = ['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if case in alphabet:
+    print('Alphabet')
+
+else:
+    print('No')
