e0081d245c2e89678ec48dabdf2204f4e76b82d3
ARRamanathan <ARRamanathan@users.noreply.github.com>
2019-02-04 09:56:53 +0530

    Update prog9.c

--- prog9.c
+++ prog9.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int num1, num2, sum=0,i=0;
+	scanf("%d %d",&num1,&num2);
+	//scanf("%d",&num2);
+	int arr[100];
+	//printf("num_2");
+	for (i=0; i<num1; i++)
+		scanf("%d" ,&arr[i]);
+	
+	for (i=0; i<num2; i++)
+		sum=sum+arr[i];
+		
+	printf("%d",sum);
+}
